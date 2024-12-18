f89cc0e048657434070fcf369b8fd21b16e9fe4b
임세원 <sewon0918@kaist.ac.kr>
2024-12-19 06:05:57 +0900

    [deploy] test: vite.config.ts base 원복

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/portfolio/",
   server: {
     host: "0.0.0.0", // 모든 IP 주소에서 접근 가능하도록 설정
     port: 5173, // 기본 포트 (선택 사항)
